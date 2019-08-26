1f4315eef45accaa987c441f2691b3bd2c003cee
josephlim_94 <josephlim_94@hotmail.co.uk>
2019-08-26 20:58:50 +0800

    encode audio with opus codec

--- application_side_routing.py
+++ application_side_routing.py
@@ -7,12 +7,17 @@ import wave
 import sys
 import time
 
-CHUNK = 1024*512
+#CHUNK = 1024*512
+CHUNK=1920
 
 wf = wave.open('test.wav', 'rb')
 
 async def hello(uri):
+    from opus import encoder
     async with websockets.connect(uri) as websocket:
+        CHANNELS = 2
+        RATE = 48000
+        encoder = encoder.Encoder(RATE,CHANNELS,'voip')
         #await websocket.send("Hello world!")
         data = wf.readframes(CHUNK)
         #await websocket.send(data)
@@ -30,7 +35,9 @@ async def hello(uri):
 
         while data:
             #stream.write(data)
-            await websocket.send(data)
+            encoded_data = encoder.encode(data, CHUNK)
+            #await websocket.send(data)
+            await websocket.send(encoded_data)
             data = wf.readframes(CHUNK)
             #response = await websocket.recv()
             #time.sleep(int(response))
@@ -43,4 +50,4 @@ async def hello(uri):
         print(total)
 
 asyncio.get_event_loop().run_until_complete(
-    hello('ws://localhost:8000/ws/speaker/audioplayback/'))
+    hello('ws://192.168.0.104:8000/ws/speaker/audioplayback/'))
--- speaker/consumers.py
+++ speaker/consumers.py
@@ -6,11 +6,18 @@ import wave
 import sys
 from . import pyaudio_asynchronous
 import multiprocessing
-
+from opus import decoder
 
 class AudioPlaybackConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
+        # Opus decoder
+        CHANNELS = 2
+        RATE = 48000
+        self.CHUNK = 1920
+        self.decoder = decoder.Decoder(RATE,CHANNELS)
+        self.opus_encoded = True
+        # PyAudio Multiprocessing Wrapper
         self.audio_packet_queue = multiprocessing.Queue()
         self.period_sync_event = multiprocessing.Event()
         pyaudio_asynchronous.start(self.audio_packet_queue,
@@ -24,4 +31,6 @@ class AudioPlaybackConsumer(WebsocketConsumer):
             print("Recieved text_data: ", len(text_data))
         if bytes_data:
             print("Recieved bytes_data: ", len(bytes_data))
+            if self.opus_encoded:
+                bytes_data = self.decoder.decode(bytes_data, self.CHUNK)
             self.audio_packet_queue.put(bytes_data)
