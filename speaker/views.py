bb714cef90269d34e71aa1c3ece59f04f0b8bc7c
josephlim_94 <josephlim_94@hotmail.co.uk>
2019-08-30 10:23:52 +0800

    record from microphone, based on brautopy

--- speaker/views.py
+++ speaker/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
 
 # Create your views here.
+
+def index(request):
+    template = loader.get_template('speaker/index.html')
+    return HttpResponse(template.render(None, request))
